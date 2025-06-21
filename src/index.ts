99fa4cf9cfd1d50f80993687e4b5404824ca90f7
Dmitriy Storozhuk <gelendzhik94@yandex.ru>
2025-06-21 15:02:59 +0300

    mod back

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import {productsRouter} from './routes/products-router';
 import {addressesRouter} from './routes/addresses-router';
 
 const app = express()
-const port = 'https://baseback-beta.vercel.app/'
+const port = process.env.PORT || 5000
 
 
 //Что бы корректно передать данные, с клиента нужно получить json и обработать, для обработки используем bodyParser from 'body-parser и ниже него вызываем для app
